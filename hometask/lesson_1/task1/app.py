01adceadfabefd19fd6c8afff030903ad193a0ab
AlexanderPolezhaev <maks310343@gmail.com>
2022-12-17 02:28:48 +0300

    Новая версия первого задания

--- hometask/lesson_1/task1/app.py
+++ hometask/lesson_1/task1/app.py
@@ -4,6 +4,6 @@ lastday = 7
 if day > lastday:
     print ("В неделе 7 дней, ты не знал?")
 elif (day == 6) or (day == 7):
-    print ("Этот день выходной")
+    print (f" {day} - Этот день выходной")
 else:
-    print ("Этот день не выходной")
+    print (f" {day} - Этот день не выходной")
