90b6785cf4fd8a1bf86420704adc80469074a9a6
AlexanderPolezhaev <maks310343@gmail.com>
2023-01-14 00:11:57 +0300

    Первое задание сделано

---
+++ hometask/lesson_4/task1/app.py
@@ -0,0 +1,15 @@
+import random
+import os
+
+def generate_polynomial(k):
+    coefficients = [random.randint(0, 100) for _ in range(k + 1)]
+    polynomial = " + ".join([f"{coefficient}*x^{i}" if i > 1 else f"{coefficient}*x" if i == 1 else f"{coefficient}" for i, coefficient in enumerate(coefficients)])
+    return polynomial
+
+k = int(input("Введите степень многочлена: "))
+polynomial = generate_polynomial(k)
+print(polynomial)
+
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'polynomial.txt')
+with open(file_path, "w") as f:
+    f.write(polynomial)
