62f7fcee08facaa6fe8297ee7d70dbd881f1c657
AlexanderPolezhaev <maks310343@gmail.com>
2023-01-28 00:33:20 +0300

    Оптимизация

--- hometask/lesson_4/task2/app.py
+++ hometask/lesson_4/task2/app.py
@@ -4,9 +4,8 @@ import os
 def write_random_polynomial(file_name, degree):
     """Записываем случайный многочлен в файл."""
     with open(file_name, 'w') as f:
-        for i in range(degree+1):
-            coefficient = random.randint(-10, 10)
-            f.write(f'{coefficient} {i}\n')
+        coefficients = [random.randint(-10, 10) for _ in range(degree+1)]
+        f.write('\n'.join([f'{c} {i}' for i, c in enumerate(coefficients)]))
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 write_random_polynomial(f'{dir_path}/polynomial1.txt', 3)
@@ -15,35 +14,19 @@ write_random_polynomial(f'{dir_path}/polynomial2.txt', 4)
 def read_polynomial(file_name):
     """Считываем многочлен из файла и возвращает его в виде словаря."""
     with open(file_name, 'r') as f:
-        polynomial = {}
-        for line in f:
-            degree, coefficient = line.strip().split()
-            polynomial[int(degree)] = int(coefficient)
-    return polynomial
+        return {int(degree): int(coefficient) for coefficient, degree in (line.strip().split() for line in f)}
 
 def write_polynomial(file_name, polynomial):
     """Записываем многочлен в файл."""
     with open(file_name, 'w') as f:
-        for degree in sorted(polynomial.keys(), reverse=True):
-            f.write(f'{polynomial[degree]} {degree}\n')
+        f.write('\n'.join([f'{polynomial[degree]} {degree}' for degree in sorted(polynomial.keys(), reverse=True)]))
 
 
 polynomial1 = read_polynomial(f'{dir_path}/polynomial1.txt')
 polynomial2 = read_polynomial(f'{dir_path}/polynomial2.txt')
 
 
-result = {}
-for degree in polynomial1.keys():
-    if degree in result:
-        result[degree] += polynomial1[degree]
-    else:
-        result[degree] = polynomial1[degree]
-
-for degree in polynomial2.keys():
-    if degree in result:
-        result[degree] += polynomial2[degree]
-    else:
-        result[degree] = polynomial2[degree]
+result = {d: polynomial1.get(d, 0) + polynomial2.get(d, 0) for d in set(polynomial1) | set(polynomial2)}
 
 
 write_polynomial(f'{dir_path}/result.txt', result)
